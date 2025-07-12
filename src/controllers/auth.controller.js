6177607adaef6c8f37461b8982c1dc8c76d243f4
skumar <kumasachin@gmail.com>
2025-07-12 21:53:03 +0100

    logout logic updated

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -95,8 +95,14 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {
   try {
-    res.cookie("jwt", "", { maxAge: 0 });
-    res.status(200).json({ message: "Logged out successfully" });
+ res.clearCookie("jwt", {
+   maxAge: 0, // Expire immediately
+   httpOnly: true,
+   sameSite: "lax",
+   secure: process.env.NODE_ENV === "production",
+   path: "/", // Make sure path matches the one used when setting the cookie
+ });
+ res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
