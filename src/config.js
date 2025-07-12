eb2f19015495f4b28cc2f6da867255a0fff326e7
skumar <kumasachin@gmail.com>
2025-07-12 19:38:22 +0100

    updated config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const LocalPath =
   process.env.NODE_ENV === "production"
-    ? process.env.FRONTEND_URL || "https://sachink.dev/chatter"
+    ? process.env.FRONTEND_URL || "https://chatterfrontend.onrender.com"
     : "http://localhost:5174";
