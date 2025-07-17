98f84e6827d085ed30356fb0157e1a367207ac55
skumar <kumasachin@gmail.com>
2025-07-17 12:19:15 +0100

    add logic to add buy

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import friendRequestRoutes from "./routes/friendRequest.route.js";
+import buyRequestRoutes from "./routes/buyRequest.route.js";
 import { app, server } from "./lib/socket.js";
 
 const allowedOrigins = [
@@ -40,6 +41,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/friend-requests", friendRequestRoutes);
+app.use("/api/buy-request", buyRequestRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
